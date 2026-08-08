19efffa74cd3197f4b2ec8be659a4d597cf8a323
piyush30073 <piyush30073@gmail.com>
2026-08-09 02:58:24 +0530

    mongoDB connection

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./.env"
+});
+
+console.log("MONGO URI exists:", !!process.env.MONGODB_URI);
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+
+const app = express();
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT, () => {
+            console.log(`APP is listening on port ${process.env.PORT}`);
+        });
+
+    } catch (error) {
+        console.error("ERROR: ", error);
+        throw error;
+    }
+})();*/
